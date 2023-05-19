15045275bd7752443b3a824e5be6ef1fbe641d27
antran <trannhatan2803@gmail.com>
2023-05-19 13:48:01 +0700

    - update toast message in PlaceOrderScreen.js

--- client/src/screens/PlaceOrderScreen.js
+++ client/src/screens/PlaceOrderScreen.js
@@ -1,21 +1,21 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { createOrder } from "../Redux/Actions/OrderActions";
-import { ORDER_CREATE_RESET } from "../Redux/Constants/OrderConstants";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {Link} from "react-router-dom";
+import {createOrder} from "../Redux/Actions/OrderActions";
+import {ORDER_CREATE_RESET} from "../Redux/Constants/OrderConstants";
 import Header from "./../components/Header";
 import Message from "./../components/LoadingError/Error";
 import {PaymentMethod} from "./PaymentScreen";
 import {toast} from "react-toastify";
 
-const PlaceOrderScreen = ({ history }) => {
+const PlaceOrderScreen = ({history}) => {
   window.scrollTo(0, 0);
 
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const userLogin = useSelector((state) => state.userLogin);
 
-  const { userInfo } = userLogin;
+  const {userInfo} = userLogin;
   const typePay = localStorage.getItem("typePay");
   const pay = JSON.parse(localStorage.getItem("paymentMethod"));
 
@@ -47,13 +47,13 @@ const PlaceOrderScreen = ({ history }) => {
   }
 
   const orderCreate = useSelector((state) => state.orderCreate);
-  const { order, success, error } = orderCreate;
+  const {order, success, error} = orderCreate;
 
   useEffect(() => {
     if (success) {
-      toast.success(`You have successfully ordered with ${pay}`)
+      toast.success(`You have successfully ordered with ${pay == PaymentMethod.PayPal ? "PayPal" : "Payment on delivery"}`)
       history.push(`/order/${order._id}`);
-      dispatch({ type: ORDER_CREATE_RESET });
+      dispatch({type: ORDER_CREATE_RESET});
     }
   }, [history, dispatch, success, order]);
 
@@ -64,7 +64,7 @@ const PlaceOrderScreen = ({ history }) => {
       dispatch(
         createOrder({
           orderItems: cart.cartItems,
-          user:userInfo,
+          user: userInfo,
           shippingAddress: cart.shippingAddress,
           paymentMethod: pay,
           itemsPrice: cart.itemsLoanPrice,
@@ -78,7 +78,7 @@ const PlaceOrderScreen = ({ history }) => {
     } else {
       dispatch(
         createOrder({
-          user:userInfo,
+          user: userInfo,
           orderItems: cart.cartItems,
           shippingAddress: cart.shippingAddress,
           paymentMethod: pay,
@@ -115,7 +115,7 @@ const PlaceOrderScreen = ({ history }) => {
 
   return (
     <>
-      <Header />
+      <Header/>
       <div className="container">
         <div className="row  order-detail">
           <div className="col-lg-4 col-sm-4 mb-lg-4 mb-5 mb-sm-0">
@@ -148,7 +148,7 @@ const PlaceOrderScreen = ({ history }) => {
                 </h5>
                 <p>Shipping: {cart.shippingAddress.country}</p>
                 <p>
-                Payments Methods:{" "}
+                  Payments Methods:{" "}
                   {cart.paymentMethod === PaymentMethod.Credit
                     ? "Payment on delivery"
                     : "PayPal"}
@@ -189,14 +189,15 @@ const PlaceOrderScreen = ({ history }) => {
                 {cart.cartItems.map((item, index) => (
                   <div className="order-product row" key={index}>
                     <div className="col-md-3 col-6">
-                      <img src={item.image} alt={item.name} />
+                      <img src={item.image} alt={item.name}/>
                     </div>
                     <div className="col-md-5 col-6 d-flex align-items-center">
                       <Link to={`/products/${item.product}`}>
                         <h6>{item.name}</h6>
                       </Link>
                     </div>
-                    <div className="mt-3 mt-md-0 col-md-2 col-6  d-flex align-items-center flex-column justify-content-center ">
+                    <div
+                      className="mt-3 mt-md-0 col-md-2 col-6  d-flex align-items-center flex-column justify-content-center ">
                       <h4>SQuantity</h4>
                       <h6>{item.qty}</h6>
                       <br/>
@@ -204,7 +205,8 @@ const PlaceOrderScreen = ({ history }) => {
                       <h6>{item.size}</h6>
                     </div>
 
-                    <div className="mt-3 mt-md-0 col-md-2 col-6 align-items-end  d-flex flex-column justify-content-center ">
+                    <div
+                      className="mt-3 mt-md-0 col-md-2 col-6 align-items-end  d-flex flex-column justify-content-center ">
                       <h4>Total</h4>
                       <h6>
                         {item.qty &&
@@ -225,30 +227,30 @@ const PlaceOrderScreen = ({ history }) => {
           <div className="col-lg-3 d-flex align-items-end flex-column mt-5 subtotal-order">
             <table className="table table-bordered">
               <tbody>
-                <tr>
-                  <td>
-                    <strong>Product</strong>
-                  </td>
-                  <td>{showPrice(cart.itemsPrice)}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Shipping</strong>
-                  </td>
-                  <td>{showPrice(cart.shippingPrice)} </td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Tax</strong>
-                  </td>
-                  <td>{showPrice(cart.taxPrice)}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Total</strong>
-                  </td>
-                  <td>{showPrice(cart.totalPrice)}</td>
-                </tr>
+              <tr>
+                <td>
+                  <strong>Product</strong>
+                </td>
+                <td>{showPrice(cart.itemsPrice)}</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Shipping</strong>
+                </td>
+                <td>{showPrice(cart.shippingPrice)} </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Tax</strong>
+                </td>
+                <td>{showPrice(cart.taxPrice)}</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Total</strong>
+                </td>
+                <td>{showPrice(cart.totalPrice)}</td>
+              </tr>
               </tbody>
             </table>
             {cart.cartItems.length === 0 ? null : (
